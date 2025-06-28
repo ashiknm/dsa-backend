a3645f42cde961e8145de0f01311f30c670a5cd4
Ashik N M <ashikagumbe@gmail.com>
2025-06-28 17:40:51 +0530

    added subdomain

--- index.js
+++ index.js
@@ -184,6 +184,7 @@ app.use(
       "http://localhost:3001",
       "https://v0-react-dsa-website-ashiks-projects-9613a13b.vercel.app",
       "https://v0-react-dsa-website.vercel.app",
+      "https://learning.ashikagumbe.com"
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
